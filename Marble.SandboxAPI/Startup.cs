510e3826e89d9c9f679b37d5f416ac4006d08098
Markus Stephanides <maste@live.at>
2020-10-28 19:48:07 +0100

    Initial develop branch commit

--- Marble.SandboxAPI/Startup.cs
+++ Marble.SandboxAPI/Startup.cs
@@ -15,21 +15,20 @@ namespace Marble.SandboxAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
             services.AddSwaggerGen();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
