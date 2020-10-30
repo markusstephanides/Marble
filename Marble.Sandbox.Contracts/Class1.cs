e0df73ebebf80112d833d21f7b20ee0d77640906
Christoph Reichhardt <chr@limeship.com>
2020-10-30 20:27:35 +0100

    add mathservice example add contracts project

---
+++ Marble.Sandbox.Contracts/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Marble.Sandbox.Contracts
+{
+    public class Class1
+    {
+    }
+}
---
+++ Marble.Sandbox/MathService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Marble.Sandbox
+{
+    // [MarbleController]
+    public class MathService
+    {
+        // [MarbleProcedure]
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        // [MarbleProcedure]
+        public Task<int> ComplexAdd(int a, int b)
+        {
+            return Task.FromResult(a + b);
+        }
+
+        // [MarbleProcedure]
+        public MathResult Something()
+        {
+            return new MathResult();
+        }
+    }
+
+    public class MathResult
+    {
+        public int SomeInt { get; set; }
+    }
+}
