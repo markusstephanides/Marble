3e3ca73f6c69e32a1b4fbe4a962e5388151acded
Markus Stephanides <maste@live.at>
2020-11-10 19:01:09 +0100

    Increment utilities version

--- Marble.Utilities/Extensions/IAppHostBuilderExtensions.cs
+++ Marble.Utilities/Extensions/IAppHostBuilderExtensions.cs
@@ -18,6 +18,13 @@ namespace Marble.Utilities.Extensions
         {
             return builder.ConfigureServices(collection => collection.AddSingleton(instance));
         }
+        
+        public static IAppHostBuilder AddSingleton<TAbstraction, TImplementation>(this IAppHostBuilder builder)
+            where TAbstraction : class
+            where TImplementation : class, TAbstraction
+        {
+            return builder.ConfigureServices(collection => collection.AddSingleton<TAbstraction, TImplementation>());
+        }
 
         public static IAppHostBuilder AddSingleton<TService>(this IAppHostBuilder builder,
             Func<TService, TService> factory)
